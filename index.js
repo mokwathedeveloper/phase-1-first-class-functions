f0421071716b82573c92c6b1f9abb2b91d7ec145
MokwaTheDeveloper <moffatmokwa12@gmail.com>
2023-09-24 21:46:29 +0300

    Completed assigment

---
+++ index.js
@@ -0,0 +1,27 @@
+//The receivesAFunction function should:
+
+//take a callback function as an argument
+//call the callback function
+//it doesn't matter what this function returns, so long as it calls the callback function
+
+
+function John() {
+  console.log("Go for a five-mile run");
+  console.log("Pump iron");
+}
+function Mary() {
+  console.log("Go for a 10-mile run");
+  console.log("Pump iron");
+}
+function Bob() {
+  console.log("Go for a 15-mile run");
+  console.log("Pump iron");
+}
+function Tom() {
+  console.log("Go for a 20-mile run");
+  console.log("Pump iron");
+}
+function jerry() {
+  console.log("Go for a 25-mile run");
+  console.log("Pump iron");
+}
